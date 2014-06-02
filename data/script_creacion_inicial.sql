a06a29168798004dfd1313f41b2356710aa1b426
gusTrucco <gus.trucco@gmail.com>
2014-06-02 15:32:46 -0300

    Agregado de Factura, ids autoincrementales..

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -6,14 +6,14 @@ GO
 
 
 CREATE TABLE SQL_O.Usuario(
-		UserId numeric(18,0) Primary Key,
+		UserId numeric(18,0) Primary Key Identity,
 		Username varchar(30) Unique,
 		Userpass char(32)
 		)
 GO
 
 CREATE TABLE SQL_O.Visibilidad(
-		Vis_Cod numeric(18,0) Primary Key,
+		Vis_Cod numeric(18,0) Primary Key Identity,
 		Vis_Desc nvarchar(255),
 		Vis_Precio numeric(18,2),
 		Vis_Porcentaje numeric(18,2)
@@ -32,7 +32,7 @@ GO
 
 
 CREATE TABLE SQL_O.Datos_Pers(
-		Datos_Id numeric(18,0) Primary Key,
+		Datos_Id numeric(18,0) Primary Key Identity,
 		Datos_Mail nvarchar(50),
 		Datos_Tel numeric(18,0),
 		Datos_Dom_Calle nvarchar(100), 
@@ -45,19 +45,12 @@ GO
 		
 
 CREATE TABLE SQL_O.Empresa(
-		Emp_Cod numeric(18,0) Primary Key references SQL_O.Rol(Rol_Cod) NOT NULL,
+		Emp_Cod numeric(18,0) Primary Key identity references SQL_O.Rol(Rol_Cod) NOT NULL,
 		Emp_Razon_Social nvarchar(255) Unique,
 		Emp_Cuit nvarchar(50) Unique,
 		Emp_Fecha_Creacion datetime,
 		Emp_Contacto nvarchar(50),
 		Emp_Datos_Pers numeric(18,0) references SQL_O.Datos_Pers(Datos_Id) NOT NULL,
-		--Emp_Mail nvarchar(50),
-		--Emp_Tel numeric(18,0),
-		--Emp_Dom_Calle nvarchar(100), 
-		--Emp_Nro_Calle numeric(18,0),
-		--Emp_Dom_Piso numeric(18,0),
-		--Emp_Depto nvarchar(50),
-		--Emp_Cod_Postal nvarchar(50),
 		Emp_Estado bit,
 		Emp_Reputacion numeric(18,0),	
 	)
@@ -65,7 +58,7 @@ Go
 
 
 CREATE TABLE SQL_O.Cliente(
-		Cli_Id numeric(18,0) Primary Key references SQL_O.Rol(Rol_Cod) NOT NULL,
+		Cli_Id numeric(18,0) Primary Key identity references SQL_O.Rol(Rol_Cod) NOT NULL,
 		Cli_NroDoc numeric(18,0), 
 		Cli_TipoDoc nvarchar(20), 
 		Cli_Apellido nvarchar(255),
@@ -73,13 +66,6 @@ CREATE TABLE SQL_O.Cliente(
 		Cli_Cuil numeric(18,0) Unique,
 		Cli_Fecha_Nac datetime,
 		Cli_Datos_Pers numeric(18,0) references SQL_O.Datos_Pers(Datos_Id) NOT NULL,
-		--Cli_Mail nvarchar(255),
-		--Cli_Tel numeric(18,0),
-		--Cli_Dom_Calle nvarchar(255),
-		--Cli_Nro_Calle numeric(18,0),
-		--Cli_Piso numeric(18,0),
-		--Cli_Depto numeric(18,0),
-		--Cli_Cod_Post nvarchar(50),
 		Cli_Estdo bit,
 		Cli_Reputacion numeric(18)
 		--Primary Key(Cli_NroDoc,Cli_TipoDoc)
@@ -87,7 +73,7 @@ CREATE TABLE SQL_O.Cliente(
 GO
 
 CREATE TABLE SQL_O.Administrativo(
-		Admin_Id numeric(18,0) Primary Key References SQL_O.Rol(Rol_Cod) NOT NULL,
+		Admin_Id numeric(18,0) Primary Key identity References SQL_O.Rol(Rol_Cod) NOT NULL,
 		Admin_NroDoc numeric(18,0), 
 		Admin_TipoDoc nvarchar(20), 
 		Admin_Apellido nvarchar(255),
@@ -102,15 +88,14 @@ GO
 		
 
 CREATE TABLE SQL_O.Tipo_Pub(
-		Tipo_Id numeric(18,0) Primary Key,
+		Tipo_Id numeric(18,0) Primary Key identity,
 		Tipo char(1),
 		Tipo_Precio numeric(18,2),
-		Tipo_Comprador numeric(18,0) references SQL_O.ROL(Rol_Cod) NOT NULL
 		)
 GO
 
 CREATE TABLE SQL_O.Publicacion(
-		Pub_Cod numeric(18,0) Primary Key,
+		Pub_Cod numeric(18,0) Primary Key identity,
 		Pub_Desc nvarchar(255),
 		Pub_Stock numeric(18,0),
 		Pub_Fecha_Ini datetime,
@@ -124,13 +109,13 @@ CREATE TABLE SQL_O.Publicacion(
 GO
 
 CREATE TABLE SQL_O.Respuesta(
-		Res_Cod numeric(18,0) Primary key,
+		Res_Cod numeric(18,0) Primary key identity,
 		Res_Texto nvarchar(255),
 		Res_Fecha datetime
 		)
 GO
 CREATE TABLE SQL_O.Pregunta(
-		Pre_Id numeric(18,0) Primary Key,
+		Pre_Id numeric(18,0) Primary Key identity,
 		Pre_Pub numeric(18,0) references SQL_O.Publicacion(Pub_Cod) NOT NULL,
 		Pre_Res numeric(18,0) references SQL_O.Respuesta(Res_Cod) NOT NULL,
 		Pre_Texto nvarchar(255),
@@ -141,24 +126,27 @@ GO
 
 
 CREATE TABLE SQL_O.Compra(
-		Compra_Id numeric(18,0) Primary Key,
+		Compra_Id numeric(18,0) Primary Key identity,
 		Compra_Pub numeric(18,0) references SQL_O.Publicacion(Pub_Cod) NOT NULL,
 		Compra_Fecha datetime,
-		Compra_Cantidad numeric(18,0)
+		Compra_Cantidad numeric(18,0),
+		Compra_Factura numeric(18,0) references SQL_O.Factura(Factura_Nro)
+		Compra_Comprador numeric(18,0) references SQL_O.Cliente(Cli_Id)
 		)
 GO
 
 CREATE TABLE SQL_O.Oferta(
-		Oferta_Id numeric(18,0) Primary Key,
+		Oferta_Id numeric(18,0) Primary Key identity,
 		Oferta_Pub numeric(18,0) references SQL_O.Publicacion(Pub_Cod) NOT NULL,
 		Oferta_Fecha datetime,
 		Oferta_Monto numeric(18,2),
 		Oferta_Cliente numeric(18,0) references SQL_O.Rol(Rol_Cod) NOT NULL
+		Oferta_Gano bit
 		)
 GO
 
 CREATE TABLE SQL_O.Calificacion(
-		Cal_Codigo numeric(18,0) Primary Key,
+		Cal_Codigo numeric(18,0) Primary Key identity,
 		Cal_Cant_Est numeric(18,0),
 		Cal_Desc nvarchar(255),
 		Cal_Pub numeric(18,0) references SQL_O.Publicacion(Pub_Cod) NOT NULL
@@ -166,7 +154,7 @@ CREATE TABLE SQL_O.Calificacion(
 GO
 
 CREATE TABLE SQL_O.Rubro(
-		Rubro_Cod numeric(18,0) Primary key,
+		Rubro_Cod numeric(18,0) Primary key identity,
 		Rubro_Desc nvarchar(255)
 		)
 GO
@@ -178,19 +166,21 @@ CREATE TABLE SQL_O.Pub_Por_Rubro(
 		)
 GO
 
+CREATE TABLE SQL_O.Factura (
+	Factura_Nro numeric(18,0) Primary Key Identity, 
+	Factura_Fecha datetime, 
+	Factura_Total numeric(18,0),
+	Factura_Forma_Pago nvarchar(255)
+	)
+	
+
 /*Notas:
 	En tabla maestra:
 		~ Las columnas que empiezan con Publ_ se refieren a dueños de publicaciones.
 		~ Las columnas que empiezan con Cli siempre estan "unidas" a ofertas o compras, o sea son clientes que ofertaron o compraron algo.
 
-TODO:
-- Analizar tipos de datos medios fruta (userpass, entre otros).
 - Tipos de usuario donde carajo iria.
-- Analizar Joined de Rol.
-- Ver Pks de c/u (Ejemplo los unique de tipo y nro Doc, razon social y cuit, con el joined) .
-- Analizar normalizacion datos personales.
-- Analizar single table de Tipo_Pub.
-- Atributo de compra "cantidad" ver como meterlo.
-- Agregar Factura.
-- Agregar Constraints Faltantes.
-- Ver que le mandamos a Admin, puede ser de ultima un single table con cliente si van a tener lo mismo.*/
+- Cuando se finaliza una subasta la misma pasa a ser una compra con el ultimo que oferto como comprador.
+- Los items de las facturas serian las compras.
+- Analizar tipos de datos medios fruta (userpass, entre otros).
+*/
