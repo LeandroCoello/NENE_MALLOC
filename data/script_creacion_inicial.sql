b94d5359900bf1aa0c61cf13cf94fa828723e461
LeandroCoello <ln.coello8@gmail.com>
2014-09-19 18:27:17 -0300

    Creacion de Tablas y delete Script.
    Le puse usuario por las dudas porque user se marcaba rosa, por ahi es
    reservada.

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -3,3 +3,205 @@ GO
 
 CREATE SCHEMA  NENE_MALLOC AUTHORIZATION gd 
 GO
+
+-- Creacion de Tablas
+
+CREATE TABLE NENE_MALLOC.Rol(
+		Rol_Id numeric(18,0) primary key identity,
+		Rol_Desc nvarchar(255),
+		Rol_estado bit default 0
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Funcionalidad(
+		Func_Id numeric(18,0) Primary Key Identity,
+		Func_Desc nvarchar(255)		
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Func_Por_Rol(
+		Rol_Id numeric(18,0)references NENE_MALLOC.Rol(Rol_Id),
+		Func_Id numeric(18,0) references NENE_MALLOC.Funcionalidad(Func_Id),	
+		Primary Key (Rol_Id,Func_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Datos_Personales(
+		Datos_Id numeric(18,0) primary key identity,
+		Datos_Nombre nvarchar(255),
+		Datos_Apellido nvarchar(255),
+		Datos_Telefono numeric(18,0),
+		Datos_Tipo_Ident nvarchar(30),
+		Datos_Nro_Ident numeric(18,0),
+		Datos_Mail nvarchar(255),
+		Datos_Dom_Calle nvarchar(255),--puse el domicilio asi por la tabla maestra
+		Datos_Dom_Nro_Calle numeric(18,0),
+		Datos_Dom_Piso numeric(18,0),
+		Datos_Dom_Depto nvarchar(50),
+		Datos_Fecha_Nac datetime
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Usuario(
+		Usuario_Id numeric(18,0) Primary Key Identity,
+		Usuario_name varchar(30) Unique,
+		Usuario_pass nvarchar (255),
+		Usuario_Intentos numeric (18,0) default 0,
+		Usuario_estado bit default 0,
+		Usuario_datos numeric(18,0) references NENE_MALLOC.Datos_Personales(Datos_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Usuario_Por_Rol(
+		Usuario_Id numeric(18,0) references NENE_MALLOC.Usuario(Usuario_Id),
+		Rol_Id numeric(18,0) references NENE_MALLOC.Rol(Rol_Id)
+		Primary Key(Usuario_Id,Rol_Id)
+	
+	)
+GO
+
+CREATE TABLE NENE_MALLOC.Hotel(
+		Hotel_Id numeric(18,0) primary key identity,
+		Hotel_Nombre nvarchar(255),
+		Hotel_Mail nvarchar(255),
+		Hotel_Telefono numeric(18,0),
+		Hotel_Calle nvarchar(255),
+		Hotel_Nro_Calle numeric(18,0),
+		Hotel_Ciudad nvarchar(255),
+		Hotel_Pais nvarchar(255),
+		Hotel_Fecha_Creacion datetime,
+		Hotel_Cerrado bit default 0,
+		Hotel_Cant_Est numeric(18,0),
+		Hotel_Recarga_Estrella numeric(18,0) -- este campo esta en la maestra, supongo que debe ser lo del calculo del costo que era un incremento que dependia de las estrellas
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Usuario_Por_Hotel(
+		Hotel_Id numeric(18,0) references NENE_MALLOC.Hotel(Hotel_Id),
+		Usuario_Id numeric(18,0) references NENE_MALLOC.Usuario(Usuario_Id),
+		Primary Key (Hotel_Id,Usuario_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Periodos_De_Cierre(
+		Periodo_Id numeric(18,0) primary key identity,
+		Periodo_Hotel numeric(18,0) references NENE_MALLOC.Hotel(Hotel_Id),
+		Periodo_FechaInicio datetime,
+		Periodo_FechaFin datetime,
+		Periodo_Motivo nvarchar(255)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Regimen(
+		Regimen_Id numeric(18,0) primary key identity,
+		Regimen_Desc nvarchar(255),
+		Regimen_Precio numeric(18,2),
+		Regimen_Inactivo bit default 0
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Regimen_Por_Hotel(
+		Hotel_Id numeric(18,0) references NENE_MALLOC.Hotel(Hotel_Id),
+		Regimen_Id numeric(18,0) references NENE_MALLOC.Regimen(Regimen_Id)
+		Primary Key(Hotel_Id,Regimen_Id)
+		)
+GO
+--Por tabla maestra agregue el tipo como otra tabla 
+CREATE TABLE NENE_MALLOC.Tipo_Habitacion(
+		Tipo_Hab_Id numeric(18,0) primary key,
+		Tipo_Hab_Desc nvarchar(255),
+		Tipo_Hab_Porc numeric(18,2)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Habitacion(
+		Habitacion_Id numeric(18,0) primary key identity,--le puse id igual, de ultima se lo sacamos
+		Habitacion_Num numeric(18,0),
+		Habitacion_Piso numeric(18,0),
+		Habitacion_Hotel numeric(18,0) references NENE_MALLOC.Hotel(Hotel_Id),
+		Habitacion_Tipo numeric(18,0) references NENE_MALLOC.Tipo_Habitacion(Tipo_Hab_Id),
+		Habitacion_Vista nvarchar(50), -- en la maestra esta como frente
+		Habitacion_Desc nvarchar(255), --esto no se, por ahi el enunciado se referia a la desc del tipo
+		Habitacion_Cerrada bit default 0
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Cliente(
+		Cliente_Id numeric(18,0) primary key identity,
+		Cliente_Datos numeric(18,0) references NENE_MALLOC.Datos_Personales(Datos_Id),
+		Cliente_Nacionalidad nvarchar(255),
+		Cliente_Deshabilitado bit default 0
+		)
+GO		
+
+CREATE TABLE NENE_MALLOC.Reserva(
+		Reserva_Id numeric(18,0) primary key,
+		Reserva_Cliente numeric(18,0) references NENE_MALLOC.Cliente(Cliente_Id),
+		Reserva_Fecha datetime,
+		Rererva_FechaIng datetime,
+		Reserva_FechaSal datetime,
+		Reserva_CantNoches numeric(18,0), --de nuevo por la maestra, despues vemos cual sacamos si fecha fin o este
+		Reserva_Estado nvarchar(255),
+		Reserva_Hotel numeric(18,0) references NENE_MALLOC.Hotel(Hotel_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Reserva_Por_Habitacion(
+		RPH_Id numeric(18,0) primary key identity, --le tuve que agregar esto porque si mandaba que Estadia_Reserva referenciara a Reserva Id de aca rompia porque habia dos pks
+		Reserva_Id numeric(18,0) references NENE_MALLOC.Reserva(Reserva_Id) not null,
+		Habitacion_Id numeric(18,0) references NENE_MALLOC.Habitacion(Habitacion_Id)not null,
+		Regimen_Id numeric(18,0) references NENE_MALLOC.Regimen(Regimen_Id)
+		Unique(Reserva_Id,Habitacion_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Estadia(
+		Estadia_Id numeric(18,0) primary key identity,
+		Estadia_Reserva numeric(18,0) references NENE_MALLOC.Reserva_Por_Habitacion(RPH_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Estadia_Por_Cliente(
+		Cliente_Id numeric(18,0) references NENE_MALLOC.Cliente(Cliente_Id),
+		Estadia_Id numeric(18,0) references NENE_MALLOC.Estadia(Estadia_Id),
+		Primary Key (Cliente_Id,Estadia_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Consumible(
+		Consumible_Id numeric(18,0) primary key,
+		Consumible_Desc nvarchar(255),
+		Consumible_precio numeric(18,2)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Consumible_Por_Estadia(
+		Consumible_Id numeric(18,0) references NENE_MALLOC.Consumible(Consumible_Id),
+		Estadia_Id numeric(18,0) references NENE_MALLOC.Estadia(Estadia_Id)
+		)
+GO
+
+--En la tabla maestra un item es una estadia o un consumible, hay que revisar esto
+CREATE TABLE NENE_MALLOC.Item_Factura(
+		Item_Factura_Id numeric(18,0) primary key identity,
+		Item_Factura_Cantidad numeric(18,0),
+		Item_Factura_Monto numeric(18,2),
+		Item_Factura_Estadia numeric(18,0) references NENE_MALLOC.Estadia(Estadia_Id)
+		)
+GO
+
+CREATE TABLE NENE_MALLOC.Factura(
+		Factura_Id numeric(18,0) primary key,
+		Factura_Cliente numeric(18,0) references NENE_MALLOC.Cliente(Cliente_Id),
+		Factura_Fecha datetime,
+		Factura_Total numeric(18,2)
+		)
+GO
+
+
+
+
+		
+
+
+
